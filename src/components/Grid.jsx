9c44575511e8105422da6c7a8cbf380f0ecbdd47
Joshua-Patton <105231202+Joshua-Patton@users.noreply.github.com>
2025-04-08 20:43:03 +0800

    feat: grid component - on hover splits

---
+++ src/components/Grid.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from '../styles/Grid.module.css';
+
+const Grid = ({ rows, columns, bgSrc}) => {
+  const gridStyle = {
+    gridTemplateRows: `repeat(${rows}, 1fr)`,
+    gridTemplateColumns: `repeat(${columns}, 1fr)`,
+  };
+  const gridItems = Array.from({ length: rows * columns }, (_, index) => {
+    let col = index%columns;
+    let row = (index-col)/rows;
+    let rowPosition = 100*(row/(rows-1)) + "%";
+    let colPosition = 100*(col/(columns-1)) + "%";
+    let position =  colPosition +" "+ rowPosition
+    let itemStyle = {
+      $hover: {borderRadius: '5%'},
+      backgroundImage: `url(${bgSrc})`,
+      backgroundPosition: position,
+      backgroundSize: rows*100 + "% " + columns*100 + "%",
+    }
+    return (<div id={"grid:"+row+"-"+col} key={index} className={styles.gridItem} style={itemStyle}>
+    </div>
+)});
+
+  return <div className={styles.gridContainer} style={gridStyle}>{gridItems}</div>;
+};
+
+export default Grid;
