3ed4ebb0cae521d1063253bbacfc535ef656faba
Joshua-Patton <105231202+Joshua-Patton@users.noreply.github.com>
2026-02-06 16:15:40 +1100

    Update getImage.js

--- src/utility/getImage.js
+++ src/utility/getImage.js
@@ -1,3 +1,8 @@
 const images = import.meta.glob('/src/content/templates&img/img/*.{png,jpg,jpeg,svg,webp}', {
     eager: true,
     import: 'default',
+});
+
+export function getImage(filename) {
+    return images[`/src/content/templates&img/img/${filename}`];
+}
