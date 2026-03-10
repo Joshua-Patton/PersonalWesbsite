babf3db4ff124ddbe79829761a8672e075648f75
Joshua-Patton <105231202+Joshua-Patton@users.noreply.github.com>
2026-03-10 16:36:29 +0700

    feat: update contact form to include hidden input for form name and fix netlify attribute

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -94,7 +94,8 @@ const About = () => {
       <h1>contact</h1>
       <br />
       <div class="about">
-        <form name="contact" method="POST" netlify>
+        <form name="contact" method="POST" data-netifly="true">
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>Your Name: <input type="text" name="name" /></label>
           </p>
