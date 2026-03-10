f284252713e482e67b45a99cfffa05187b0adf7e
Joshua-Patton <105231202+Joshua-Patton@users.noreply.github.com>
2026-03-10 16:22:16 +0700

    feat: add contact form to About page and style it for better usability

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -93,9 +93,22 @@ const About = () => {
       <br />
       <h1>contact</h1>
       <br />
-      Feel free to reach out with any questions,
-      <br />
-      email: <strong><a href="mailto:123joshuap@gmail.com">123joshuap@gmail.com</a></strong>
+      <div class="about">
+        <form name="contact" method="POST" netlify>
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
     </div >
 
   );
